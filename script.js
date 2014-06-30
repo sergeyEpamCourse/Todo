3cf0a211dad9a660b4ad125b09ab0c9e9e3604e7
sergei <ggupmstud@gmail.com>
2014-06-30 13:27:29 +0300

    add div word-break

--- script.js
+++ script.js
@@ -87,7 +87,7 @@
 				var text = todoArr[i].text;
 				if (text === "")
 					text = "Empty todo.";
-				var $todoText = $("<pre />", {
+				var $todoText = $("<div />", {
 					'class' : "todo_text",
 				}).text(text);
 				var $closeButton = $("<span />", {
